dbcae01a497e64250dd7de1bca36f73db41b5319
huyhoang294 <hoang.do@netwpower.no>
2020-07-22 08:15:53 +0700

    chart generate

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -150,9 +150,9 @@ router.get("/chart", (req, res) => {
     twentythree: 0,
   };
 
-  const beginOfDate = moment(req.query.date).format('YYYY-MM-DD 00:00').unix();
-  const endOfDate = moment(req.query.date).format('YYYY-MM-DD 23:59').unix();
-console.log(req.query.date);
+  const beginOfDate = moment(req.query.date).format('DD-MM-YYYY 00:00');
+  // const endOfDate = moment(req.query.date).format('YYYY-MM-DD 23:59').unix();
+console.log(beginOfDate);
 // console.log(beginOfDate);
   const stationId = req.query.stationId;
   Open_log.find({ station_id: stationId }, (err, logs) => {
